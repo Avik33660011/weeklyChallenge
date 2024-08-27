32381729f52329a1f7516e6b6781667f276f969f
Avik Chakraborty <67012160+Avik33660011@users.noreply.github.com>
2024-08-27 12:40:51 +0530

    Update agecalculator.cpp

--- agecalculator.cpp
+++ agecalculator.cpp
@@ -573,3 +573,33 @@ public class Employee {
     }
 }
 
+//L4Q10
+
+public class Main {
+    public enum Day {
+        SUNDAY(0),
+        MONDAY(1),
+        TUESDAY(2),
+        WEDNESDAY(3),
+        THURSDAY(4),
+        FRIDAY(5),
+        SATURDAY(6);
+
+        private final int dayNumber;
+
+        Day(int dayNumber) {
+            this.dayNumber = dayNumber;
+        }
+
+        public int getDayNumber() {
+            return dayNumber;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (Day day : Day.values()) {
+            System.out.println(day.getDayNumber() + " " + day);
+        }
+    }
+}
+
